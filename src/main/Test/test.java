e29ffc5c9ac99c5b0247f6a14da0e2ba7006deba
zhuzi01 <13237079168@163.com>
2018-08-16 14:07:10 +0800

    test

--- src/main/Test/test.java
+++ src/main/Test/test.java
@@ -4,9 +4,6 @@ public class test {
 
     @Test
     public void test1(){
-        String s1='a'+3+"helllo";
-        String s2='a'+"hello"+3;
-        System.out.println(s1);
-        System.out.println(s2);
+
     }
 }
